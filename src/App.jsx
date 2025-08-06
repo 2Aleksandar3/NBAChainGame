ace06b96af08114c5b1c77d13e9d6d2f2da413f9
Aleksandar <prodanicaleksandar@gmail.com>
2025-08-06 09:40:43 +0200

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
     <>
-      <Router>
+      <Router basename="/NBAChainGame">
         <Routes>
           <Route path="/" element={<SinglePlayer />} />
           <Route path="/multiplayer" element={<Multiplayer />} />
